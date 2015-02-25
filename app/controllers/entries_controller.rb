bb8fed11b38995ac5f831f6b5cdb620fe913075d
RosaChoi <rosa.uniq@gmail.com>
2015-02-25 10:06:25 -0800

    add rails_12factor gem for heroku deployment

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,4 +4,24 @@ class EntriesController < ApplicationController
     entries = Entry.all
     render json: entries
   end
+
+  def create
+    entry = Entry.new(entry_params)
+    if entry.save
+      render json: entry
+    else
+      render json: entry.error, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def entry_params
+    params.require(:entry).permit(
+      :title,
+      :location,
+      :description,
+      :photo_url,
+      :year
+    )
+  end
 end
