5681712297cf9ec3c1b5553cc8b7b8195ed42d9c
RosaChoi <rosa.uniq@gmail.com>
2015-02-27 00:58:17 -0800

    add seed data and comment controll for create, destry and update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,26 @@
 class CommentsController < ApplicationController
 
-  
+  def create
+    puts params
+    # entry = Entry.find(params[:comment][:entry_id])
+    comment = Comment.new(comment_params)
+    # comment = Comment.new(comment_params)
+    if comment.save
+      render json: comment
+    end
+  end
+
+  def destroy
+    Comment.find(params[:id]).destroy
+    head :no_content
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+    if comment.update(comment_params)
+      render json: comment
+    end
+  end
 
   private
   def comment_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
 Entry.create(
 title: 'The Empty Quarter',
 username: 'momoLove',
@@ -24,6 +23,42 @@ description: 'I went ballooning over the dwellings and countyside in Cappadocia.
 photo_url: 'http://i.imgur.com/nZIeuxS.jpg'
 )
 
+Entry.create(
+title: 'Cameron Highlands',
+username: 'Momo\'s Human',
+location: 'Cameron Highlands, Malaysia',
+year: '2012',
+description: 'Cameron Highlands is a beautiful holiday destination and the only place where you can find tea plantations and strawberries in Malaysia.',
+photo_url: 'http://i.imgur.com/qgqFx9O.jpg'
+)
+
+Entry.create(
+title: 'SOLITUDE',
+username: 'MomoEskie',
+location: 'Andalusia, Spain',
+year: '2012',
+description: 'Hers is no common emptiness, but a vaster silence filled with terns’ cries, an abundant solitude.',
+photo_url: 'http://i.imgur.com/TKClrrs.jpg'
+)
+
+Entry.create(
+title: 'Christmas in Nuremberg',
+username: 'Momo\'s Human',
+location: 'Nuremberg, Germany',
+year: '2011',
+description: 'The best Christmas market I\'ve ever visited! Glühwein is a must try when visiting the christmas markets. The temperature outside is chilling and the best remedy is a hot drink in your belly. Made from red wine heated with fruit and spices. Commonly used spices are cinnamon, anise, cloves, and vanilla.',
+photo_url: 'http://i.imgur.com/RlRs9f3.jpg'
+)
+
+Entry.create(
+title: 'Dhow Cruise',
+username: 'Momo\'s Human',
+location: 'Khasab, Oman',
+year: '2010',
+description: 'Khasab offers the perfect starting point to explore the unknown Musandam Peninsula and its special and unique traditions and culture.',
+photo_url: 'http://i.imgur.com/g9klg2T.jpg'
+)
+
 Comment.create(
 username: 'Fry',
 body: 'Have you seen camels?',
@@ -37,7 +72,31 @@ entry_id: 1
 )
 
 Comment.create(
-username: 'Momo',
+username: 'MomoEskie',
 body: 'It is beautiful! WOOF',
 entry_id: 2
+)
+
+Comment.create(
+username: 'MomoEskie',
+body: 'The view over this tea plantation is breathtaking.',
+entry_id: 3
+)
+
+Comment.create(
+username: 'Fry',
+body: 'Bender and I can use some Glühwein.',
+entry_id: 5
+)
+
+Comment.create(
+username: 'Momo\'s Human',
+body: 'LOL!',
+entry_id: 5
+)
+
+Comment.create(
+username: 'momoLove',
+body: 'I miss snow!!',
+entry_id: 5
 )
